5f2c83bc84630d4dab61d1145244f38aff8889dd
mgmgk1994 <mgmgk1994@gmail.com>
2017-05-09 15:24:50 +0630

    add employer_dashbord and post new job

--- app/src/main/java/com/goldenictsolutions/win/jobready365_/employer/EmprDashboard.java
+++ app/src/main/java/com/goldenictsolutions/win/jobready365_/employer/EmprDashboard.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.goldenictsolutions.win.jobready365_.R;
 
@@ -16,11 +19,26 @@ public class EmprDashboard extends Fragment {
         // Required empty public constructor
     }
 
-
+Button addcomp_pro;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empr_dashboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_empr_dashboard, container, false);
+        Spinner spinner;
+        String [] aa={"rockable","aa","bb"};
+        spinner=(Spinner)view.findViewById(R.id.businessspinner);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this.getContext(),android.R.layout.simple_dropdown_item_1line
+                ,aa);
+        spinner.setAdapter(arrayAdapter);
+        addcomp_pro=(Button)view.findViewById(R.id.empr_dash_add);
+        addcomp_pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+return view;
     }
 }
