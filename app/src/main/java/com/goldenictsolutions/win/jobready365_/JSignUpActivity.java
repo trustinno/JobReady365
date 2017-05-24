c32dfce135198513a967c6912d1a5ab60930ae1a
mgmgk1994 <mgmgk1994@gmail.com>
2017-05-24 14:13:23 +0630

    add icon for

--- app/src/main/java/com/goldenictsolutions/win/jobready365_/JSignUpActivity.java
+++ app/src/main/java/com/goldenictsolutions/win/jobready365_/JSignUpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,7 +14,8 @@ import android.widget.TextView;
 import com.goldenictsolutions.win.jobready365_.employer.Emprcompro;
 
 public class JSignUpActivity extends AppCompatActivity {
-
+/// tested ksw
+    //  asdklfjökldasjflkjaskljf
     private Button btnLogin;
     private TextView textView;
   @Override
